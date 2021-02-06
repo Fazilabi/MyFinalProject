214e73579c4b78683f4a42b100ac601d4828da7e
Fazilabi <fazilag@code.edu.az>
2021-02-07 00:27:08 +0400

    9cu ders.2ci mola sonrasi

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -9,8 +9,20 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            // ProductTestGetByUnitPrice();
+
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+
+            }
+        }
+
+        private static void ProductTestGetByUnitPrice()
+        {
             ProductManager productManager = new ProductManager(new EfProductDal());
-            foreach (var product in productManager.GetByUnitPrice(40, 100)) 
+            foreach (var product in productManager.GetByUnitPrice(15, 25))
             {
                 Console.WriteLine(product.ProductName);
 
